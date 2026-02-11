07708a0712cbe9a60c6d6d35a53fa83b5632df78
jin-kimhong646 <kimhong.chhour@student.cadt.edu.kh>
2026-02-12 00:13:09 +0700

    BLA-002 – Implement BlaButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,19 @@
 
-void main() {
+import 'package:blahblah/ui/screens/blaButton.dart';
+//import 'package:blahblah/ui/screens/test_screen.dart';
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ScreenView(),
+    );
+  }
 }
